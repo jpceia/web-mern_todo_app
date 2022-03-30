970dec3f72a097c69f740c182fb8eb63ce673615
João Ceia <joao.p.ceia@gmail.com>
2022-03-31 00:30:25 +0100

    serving react app from node server

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import routes from './routes/api.js';
+import path from 'path';
 import { config } from 'dotenv';
+
+const __dirname = path.resolve();
 
 config();
 
@@ -11,21 +14,31 @@ const port = process.env.PORT || 5000;
 
 // Connect to the database
 mongoose
-    .connect(process.env.DB, { useNewUrlParser: true })
+    .connect(process.env.DB, {
+        useNewUrlParser: true,
+        // useUnifiedTopology: true,
+    })
     .then(() => console.log('Database connected successfully'))
     .catch((err) => console.log(err));
 
 // Since mongoose's Promise is deprecated, we override it with Node's Promise
 mongoose.Promise = global.Promise;
 
+// Middleware
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-
 app.use(bodyParser.json());
 
+// Serve static assets if in production
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+
+// API routes
 app.use('/api', routes);
 
 app.use((err, req, res, next) => {

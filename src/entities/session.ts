2c6c4c499f5067d5879d7204507bb66511586f29
Joao Ceia <joao.p.ceia@gmail.com>
2023-06-25 03:21:19 +0100

    setting table name to sessions

--- src/entities/session.ts
+++ src/entities/session.ts
@@ -1,7 +1,7 @@
 import { ISession } from "connect-typeorm";
 import { Column, DeleteDateColumn, Entity, Index, PrimaryColumn } from "typeorm";
 
-@Entity()
+@Entity('sessions')
 export class Session implements ISession {
     @Index()
     @Column("bigint")

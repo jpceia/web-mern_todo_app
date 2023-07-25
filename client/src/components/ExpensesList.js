92c95db1b052aee46aaac6bcf1d262ddc986b18a
Joao Ceia <joao.p.ceia@gmail.com>
2023-07-25 19:25:18 +0100

    recomposing ExpensesList into ExpensesList and ExpensesRow

--- client/src/components/ExpensesList.js
+++ client/src/components/ExpensesList.js
@@ -1,5 +1,7 @@
+import { format } from 'date-fns';
 import React from 'react';
 import styled from 'styled-components';
+import { useDeleteExpenseMutation } from '../api/apiSlice';
 
 const Table = styled.table`
     width: 100%;
@@ -23,7 +25,40 @@ const TableRow = styled.tr`
     background-color: ${(props) => (props.isEven ? '#ddd' : '#ffffff')};
 `;
 
+const ExpensesListRow = ({ expense, isEven }) => {
+    const { id, date, category, value, description } = expense;
+    const [ deleteExpense, {
+        isLoading: isDeleting
+    }] = useDeleteExpenseMutation(id);
+
+    if (isDeleting)
+        return (
+            <TableRow isEven={isEven}>
+                <TableData colSpan="5">Deleting...</TableData>
+            </TableRow>
+        );
+    // format date to "YYYY-MM-DD"
+    const paymentDate = format(new Date(date), "dd-MM-yyyy");
+    const amount = Intl.NumberFormat(
+        'en-US', {
+            style: 'currency',
+            currency: 'EUR',
+            maximumFractionDigits: 2
+        }).format(value);
+     
+    return (
+        <TableRow isEven={isEven}>
+            <TableData>{paymentDate}</TableData>
+            <TableData>{category}</TableData>
+            <TableData>{amount}</TableData>
+            <TableData>{description}</TableData>
+            <TableData><button onClick={() => deleteExpense(id)}>❌</button></TableData>
+        </TableRow>
+    );
+}
+
 const ExpensesList = ({ expenses }) => {
+
     return (
         <Table>
             <thead>
@@ -32,20 +67,17 @@ const ExpensesList = ({ expenses }) => {
                     <TableHeader>Category</TableHeader>
                     <TableHeader>Amout</TableHeader>
                     <TableHeader>Description</TableHeader>
+                    <TableHeader></TableHeader>
                 </tr>
             </thead>
             <tbody>
-                {expenses.map((expense, index) => {
-                    const { paymentDate, category, amount, description } = expense;
-                    return (
-                        <TableRow key={index} isEven={index % 2 === 0}>
-                            <TableData>{paymentDate}</TableData>
-                            <TableData>{category}</TableData>
-                            <TableData>{amount}</TableData>
-                            <TableData>{description}</TableData>
-                        </TableRow>
-                    );
-                })}
+                {expenses.map((expense, index) => (
+                    <ExpensesListRow
+                        key={index}
+                        expense={expense}
+                        isEven={index % 2 === 0}
+                    />
+                ))}
             </tbody>
         </Table>
     );

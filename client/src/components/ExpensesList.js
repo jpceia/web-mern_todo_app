f432ea65fecfa0e7cfb9b6c7e80783bf2fde0602
Joao Ceia <joao.p.ceia@gmail.com>
2023-07-29 16:07:47 +0100

    replaced custom css table with mui/material components

--- client/src/components/ExpensesList.js
+++ client/src/components/ExpensesList.js
@@ -1,49 +1,22 @@
-import { format } from 'date-fns';
 import React, { useState } from 'react';
 import { useDeleteExpenseMutation, useGetExpensesQuery } from '../api/expenseApi';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import { DataGrid, GridActionsCellItem } from '@mui/x-data-grid';
+import DeleteIcon from '@mui/icons-material/DeleteOutlined';
+import { Paper } from '@mui/material';
 
-
-const ExpensesListRow = ({ expense }) => {
-    const { id, date, category, value, description } = expense;
-    const [ deleteExpense, {
-        isLoading: isDeleting
-    }] = useDeleteExpenseMutation(id);
-
-    if (isDeleting)
-        return (
-            <TableRow>
-                <TableCell colSpan="5">Deleting...</TableCell>
-            </TableRow>
-        );
-    // format date to "YYYY-MM-DD"
-    const paymentDate = format(new Date(date), "dd-MM-yyyy");
-    const amount = Intl.NumberFormat(
-        'en-US', {
-            style: 'currency',
-            currency: 'EUR',
-            maximumFractionDigits: 2
-        }).format(value);
-     
-    return (
-        <TableRow>
-            <TableCell>{paymentDate}</TableCell>
-            <TableCell>{category}</TableCell>
-            <TableCell>{amount}</TableCell>
-            <TableCell>{description}</TableCell>
-            <TableCell><button onClick={() => deleteExpense(id)}>❌</button></TableCell>
-        </TableRow>
-    );
+const DeleteItem = (id) => {
+    const [ deleteExpense ] = useDeleteExpenseMutation(id);
+                
+    return [<GridActionsCellItem
+        icon={<DeleteIcon />}
+        label="Delete"
+        onClick={deleteExpense}
+    />]
 }
 
+
 const ExpensesList = () => {
-    const [page, setPage] = useState(1);
+    const [page, _] = useState(1);
     const {
         data: expenses,
         isLoading,
@@ -65,28 +38,56 @@ const ExpensesList = () => {
     if (!isSuccess)
         return null;
 
+    const columns = [
+        {
+            field: 'date',
+            headerName: 'Payment Date',
+            type: 'date',
+            width: 150,
+            editable: true,
+            valueFormatter: ({ value }) => new Date(value).toLocaleDateString(),
+        },
+        {
+            field: 'category',
+            headerName: 'Category',
+            width: 150,
+            editable: true,
+        },
+        {
+            field: 'value',
+            headerName: 'Amount',
+            type: 'currency',
+            width: 130,
+            editable: true,
+            valueFormatter: ({ value }) => `$${value.toFixed(2)}`,
+        },
+        {
+            field: 'description',
+            headerName: 'Description',
+            sortable: false,
+            width: 200,
+        },
+        {
+            field: 'action',
+            headerName: 'Action',
+            sortable: false,
+            width: 120, // Adjust the width as needed
+            renderCell: ({ id }) => <DeleteItem id={id} />,
+        },
+    ];
+
     return (
-        <TableContainer component={Paper}>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Payment date</TableCell>
-                        <TableCell>Category</TableCell>
-                        <TableCell>Amout</TableCell>
-                        <TableCell>Description</TableCell>
-                        <TableCell></TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {expenses.data.map((expense, index) => (
-                        <ExpensesListRow
-                            key={index}
-                            expense={expense}
-                        />
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+        <div style={{ width: '100%', height: 400, display: 'flex', justifyContent: 'center' }}>
+            <Paper style={{ width: '90%' }}>
+                <DataGrid
+                    rows={expenses.data}
+                    loading={isLoading}
+                    columns={columns}
+                    checkboxSelection
+                    disableSelectionOnClick
+                />
+            </Paper>
+        </div>
     );
 };
 

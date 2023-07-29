6129e02c0e6395f553ee898fb715802b9205247b
Joao Ceia <joao.p.ceia@gmail.com>
2023-07-29 16:09:47 +0100

    refactored layout to use mui material components

--- client/src/shared/layout.js
+++ client/src/shared/layout.js
@@ -1,51 +1,34 @@
-import styled from 'styled-components';
-import { logout } from '../auth/authSlice';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-
-const NavbarContainer = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 60px;
-    background-color: #f2f2f2;
-    padding: 0 16px;
-`;
-
-const ProjectTitle = styled.h1`
-    margin: 0;
-    font-size: 20px;
-`;
-
-const UserLogo = styled.img`
-    width: 40px;
-    height: 40px;
-    border-radius: 50%;
-`;
-
-const Spacer = styled.div`
-    flex-grow: 1;
-`;
-
+import { AppBar, Toolbar, Typography, IconButton, Avatar } from '@mui/material';
+import { Logout } from '@mui/icons-material';
+import { logout } from '../auth/authSlice';
 
 const Layout = ({ children }) => {
     const dispatch = useDispatch();
     const { profile, loading } = useSelector((state) => state.auth);
-    if (loading)
-        return <div>Loading...</div>
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
     return (
         <>
-            <NavbarContainer>
-                <ProjectTitle>Expense Tracker</ProjectTitle>
-                <Spacer />
-                { profile && (
-                    <button onClick={() => dispatch(logout())}>
-                        Logout
-                    </button>)
-                }
-            </NavbarContainer>
-            <div>{ children }</div>
+            <AppBar position="static" sx={{ height: 60, backgroundColor: '#212121' }}>
+            <Toolbar sx={{ display: 'flex', alignItems: 'center', padding: '0 16px' }}>
+                <Typography variant="h6" component="h1" sx={{ fontSize: 20, flexGrow: 1, color: '#ffffff' }}>
+                Expense Tracker
+                </Typography>
+                {profile && (
+                <IconButton onClick={() => dispatch(logout())} color="inherit">
+                    <Logout sx={{ color: '#ffffff' }} />
+                </IconButton>
+                )}
+            </Toolbar>
+            </AppBar>
+            <div>{children}</div>
         </>
-    )
-}
+    );
+};
 
 export default Layout;
